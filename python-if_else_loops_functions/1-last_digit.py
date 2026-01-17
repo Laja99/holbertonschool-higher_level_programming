d300a0b95a2c384c6a458d0f83b668d2529a6f30
Layla Alshehri <layla.jaber.m@gmail.com>
2026-01-17 14:40:29 +0300

    fix nigative last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,10 @@
 import random
 
 number = random.randint(-10000, 10000)
-last_digit = str(number)[-1]
-last_digit = int(last_digit)
+last_digit = abs(number) % 10
+if number < 0:
+    last_digit = -last_digit
+
 if last_digit > 5:
     situation = "and is greater than 5"
 elif last_digit < 6 and last_digit != 0:
