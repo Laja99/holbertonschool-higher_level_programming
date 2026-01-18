bb75c5dfb365651de3418a5eea3b7f5360083598
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-18 12:02:26 +0300

    pycodestyle validation

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+
 def magic_calculation(a, b):
     # Import only 'add' and 'sub' from magic_calculation_102
     from magic_calculation_102 import add, sub
-    
+
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
@@ -11,4 +12,3 @@ def magic_calculation(a, b):
         return c
     else:
         return sub(a, b)
-    
