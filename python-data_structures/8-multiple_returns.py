50d8867f32b4cf193a922e19a3ca8996eef0fac9
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-20 06:50:49 +0300

    fix it

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,6 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if len(sentence) == 0:
-        first_ch = "None"
+        first_ch = (0, None)
     else:
-        first_ch = sentence[0]
-        return (length, first_ch)
+        return (length, sentence[0])
