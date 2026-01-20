2fba990e28d47afa7b554f76fae0b8364d1be21d
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-20 16:41:42 +0300

    add

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,9 +3,7 @@ def uniq_add(my_list=[]):
     new_list = []
     total = 0
     for i in my_list:
-        if my_list.count(i) > 1:
-            continue
-        else:
+        if i not in new_list:
             new_list.append(i)
             total += i
     return total
