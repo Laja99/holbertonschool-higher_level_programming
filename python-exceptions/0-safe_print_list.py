180645f0c973270306ffecd71269cf85525b7f19
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-22 15:57:10 +0300

    add commit

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    """Print the first x elements of a list safely.
 
+    Args:
+        my_list (list): List of any elements
+        x (int): Number of elements to print
+
+    Returns:
+        int: Number of elements printed
+    """
     printed = 0
-    for i in range(0, x):
+    for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
             printed += 1
-        except:
-            continue
+        except IndexError:
+            break
     print()
     return printed
