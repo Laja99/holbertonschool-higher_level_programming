9215a2e07f0775e968f8bc5e868d9924b043d6d4
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-22 15:09:05 +0300

    return my code

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    i = 0
-    while i < x:
+    for i in range(x):
         try:
-            if type(my_list[i]) == int:
+            if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
-        except IndexError:
+        except (IndexError, ValueError):
             break
-        i += 1
-
     print()
     return count
