04808763a9ab2006f9dc759a05b8420bca5b5e1c
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-23 19:47:31 +0300

    fix error

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -25,11 +25,11 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists of integers/floats)")
     if len(matrix) == 0 or any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if any(not all(isinstance(el, (int, float)) for el in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists of integers/floats)")
 
     new_matrix = []
     for row in matrix:
