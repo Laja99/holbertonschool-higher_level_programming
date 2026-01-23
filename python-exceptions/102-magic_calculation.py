15ac1a6e943722696d6b3c19e39b851ed8f01006
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-23 23:00:07 +0300

    fix it

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -3,14 +3,6 @@
 
 def magic_calculation(a, b):
     """
-    Performs a specific calculation:
-    - If a < b, returns the sum of squares from a to b - 1.
-    - Otherwise, returns the sum a + b.
+    Returns a ** + b ** if a < b, else returns a + b.
     """
-    if a < b:
-        result = 0
-        for i in range(a, b):
-            result += i ** 2
-        return result
-    else:
-        return a + b
+    return (a ** b) + (b ** a) if a < b else a + b
