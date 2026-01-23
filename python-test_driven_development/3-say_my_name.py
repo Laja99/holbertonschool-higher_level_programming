2ccfe8d85962c3188e17e7df1cde49a241ede182
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-23 20:54:32 +0300

    say my name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module for printing name
+Contains function to print "My name is <first name> <last name>"
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is <first_name> <last_name>"
+
+    Args:
+        first_name (str): The first name
+        last_name (str, optional): The last name. Defaults to "".
+
+    Raises:
+        TypeError: If first_name is not a string
+        TypeError: If last_name is not a string
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}".strip())
