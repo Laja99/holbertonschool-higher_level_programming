b1ae25c058484c123363efbf135467a8734d28ea
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-23 19:05:43 +0300

    fix place of ( in typeerror

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -36,7 +36,7 @@ def matrix_divided(matrix, div):
     if len(matrix) == 0 or any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if any(not all(isinstance(el, (int, float)) for el in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists of integers/floats)")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
     new_matrix = []
     for row in matrix:
