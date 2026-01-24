a0d0adef3a9f1bfd2ba71217a4a1794d49850904
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-24 21:42:26 +0300

    fix without try

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -11,12 +11,6 @@ import numpy as np
 def lazy_matrix_mul(m_a, m_b):
     """Multiply two matrices using NumPy"""
 
-    try:
-        result = np.matmul(m_a, m_b)
-    except ValueError:
-        raise ValueError("m_a and m_b can't be multiplied")
-    except TypeError:
-        e = "m_a and m_b must be lists of lists of integers or floats"
-        raise TypeError(e)
 
+    result = np.matmul(m_a, m_b)
     return result
