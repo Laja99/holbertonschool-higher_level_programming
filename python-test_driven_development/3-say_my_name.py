ceb76903ea97b7f98469f65bd96ee1153d6251c9
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-24 20:46:09 +0300

    fix

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -12,5 +12,7 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+    if first_name is None or last_name is None:
+        raise TypeError("first_name must be a string or last_name must be a string")
 
     print("My name is", first_name, last_name)
