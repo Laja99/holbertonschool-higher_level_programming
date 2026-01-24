6b9a3474d925088a854d2bedf36712c4289f0a78
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-24 19:23:33 +0300

    end

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -16,8 +16,8 @@ def say_my_name(first_name, last_name=""):
     if first_name and last_name:
         print(f"My name is {first_name} {last_name}")
     elif first_name:
-        print(f"My name is {first_name} ", end="")
+        print(f"My name is {first_name}  ", end="")
     elif last_name:
-        print(f"My name is {last_name} ", end= "")
+        print(f"My name is {last_name} ", end="")
     else:
         print("My name is")
