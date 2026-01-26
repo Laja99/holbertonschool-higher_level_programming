a5768163269a26b6e3c12ffe5a18c4748d535b19
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-26 08:51:05 +0300

    dealing with typeerror and valueerror

---
+++ python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Square generation module for Python project
+"""
+
+
+class Square:
+    """ class Square that defines a square by multiply size"""
+
+    def __init__(self, size=0):
+        """
+        Docstring for __init__
+        :param size=0: for Square
+
+        Raises:
+        raise ValueError : if size < 0
+        raise TypeError : if size not intger
+        """
+        if size > 0:
+            self.__size = size
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
