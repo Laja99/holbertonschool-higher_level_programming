5c5437268809876423aec9b3d125b546cf908774
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-26 08:41:32 +0300

    fix pycodestyle

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,7 +7,6 @@ Square generation module for Python project
 class Square:
     """ class Square that defines a square by multiply size"""
 
-
     def __init__(self, size):
         """
         Docstring for __init__
