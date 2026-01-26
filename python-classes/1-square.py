c9c02f05426472aae6385567a619bbe6bee9c992
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-26 08:28:01 +0300

    class Square with Private instance attribute: size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,5 +5,12 @@ Square generation module for Python project
 
 
 class Square:
-    """ class Square that defines a square"""
+    """ class Square that defines a square by multiply size"""
     __size = 0
+
+    def __init__(self, size):
+        """
+        Docstring for __init__
+        :param size: for Square
+        """
+        size = self.__size
