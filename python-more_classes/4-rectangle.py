9f58994d0642fa2c701abd9c06c07aeeb175cc2a
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-30 23:32:11 +0300

    return a string representation of the rectangle

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+Rectangle class module
+"""
+
+
+class Rectangle:
+    """
+    Class Rectangle that defines a rectangle by width and height
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Initialize a rectangle instance with optional width and height
+        Uses setters to validate values
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter for the width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for the width attribute with validation"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for the height attribute with validation"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the rectangle area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the rectangle perimeter, 0 if width or height is 0"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """
+        Return a string representation of the rectangle using #
+        Empty string if width or height is 0
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rect_lines = ["#" * self.__width for _ in range(self.__height)]
+        return "\n".join(rect_lines)
+
+    def __repr__(self):
+        """
+        Return a string that can recreate a new instance using eval()
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
