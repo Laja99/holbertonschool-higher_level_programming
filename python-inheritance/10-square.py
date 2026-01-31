eb44ec3e2f46db2af23fe1390a3082c28df21261
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-31 09:54:14 +0300

    fix it

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""Module that defines a Square class inheriting from Rectangle."""
+"""
+Defines a Square class that inherits from Rectangle
+"""
 
-Rectangle = __import__('8-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represents a square."""
+    """Represents a square, inheriting from Rectangle"""
 
     def __init__(self, size):
-        """Initialize a square with validated size."""
+        """
+        Initializes a square with a given size.
+
+        Args:
+            size (int): The size of the square's sides.
+        """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """Return the area of the square."""
-        return self.__size * self.__size
-
-    def __str__(self):
-        """Return a printable description of the square."""
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        """Returns the area of the square"""
+        return self.__size ** 2
