2548c974000ac42f17115740d74ecb86b6a5479a
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-01-31 11:31:19 +0300

    fix pycodestyle

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,21 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Dragon with Swim and Fly abilities using mixins."""
+
+
 class SwimMixin:
+    """Mixin that adds swimming ability to a creature."""
     def swim(self):
         print("The creature swims!")
 
 
 class FlyMixin:
+    """Mixin that adds flying ability to a creature."""
     def fly(self):
         print("The creature flies!")
 
 
 class Dragon(SwimMixin, FlyMixin):
+    """Dragon class that inherits from both SwimMixin and FlyMixin."""
     def roar(self):
+        """Make the dragon roar."""
         print("The dragon roars!")
-
-
-if __name__ == "__main__":
-    dragon = Dragon()
-    dragon.swim()
-    dragon.fly()
-    dragon.roar()
