87450d641cd497d56eaf1ebac8ad0be49205eaee
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-02-07 22:31:08 +0300

    pickle module

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Pickling custom classes"""
+
+import pickle
+
+
+class CustomObject:
+    """Defines a custom object"""
+
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Display object attributes"""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """Serialize the object and save it to a file"""
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except Exception:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize an object from a file and return it"""
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except Exception:
+            return None
