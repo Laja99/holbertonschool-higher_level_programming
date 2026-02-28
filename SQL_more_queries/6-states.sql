ed0ffc4f7da616af55782c142baced0cd7b1d473
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-02-28 15:57:28 +0300

    with  a primary key

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Creates the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Creates the table states with an auto-generated primary key
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    name VARCHAR(256) NOT NULL
+);
