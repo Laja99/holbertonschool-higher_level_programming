a1c26744b0c1c4404f091e7d6ae644019d9e4ab8
Layla_Alshehri <layla.jaber.m@gmail.com>
2026-02-28 16:30:05 +0300

    inner join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,5 @@
 -- Lists all cities with their states
-SELECT cities.id, cities.name, (SELECT states.name FROM states WHERE states.id = cities.state_id) FROM cities ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
