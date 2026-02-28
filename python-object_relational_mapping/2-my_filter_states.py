91b965ddaab7fa768b554480b896fa94a80a5a14
laja99 <layla.jaber.m@gmail.com>
2026-03-01 00:01:19 +0300

    Displays all values in the states table where name matches the argument

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+Displays all values in the states table where name matches the argument.
 """
 
 import MySQLdb
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    state_name = sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
@@ -21,7 +22,10 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+        state_name
+    )
+    cur.execute(query)
 
     rows = cur.fetchall()
     for row in rows:
