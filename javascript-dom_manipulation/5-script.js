4c45a44e02bd65380d9d91ad88561d889b674e1f
laja99 <layla.jaber.m@gmail.com>
2026-04-02 09:07:53 +0300

    Change the text

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const updateHeader = document.getElementById('update_header');
+const header = document.querySelector('header');
+
+updateHeader.addEventListener('click', function () {
+  header.textContent = 'New Header!!!';
+});
